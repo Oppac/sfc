b008840c9d4051458af4b2c05970f5f9be790242
Oppac <oppac26@gmail.com>
2018-10-29 21:20:15 +0100

    grammar correction

--- src/Parser.java
+++ src/Parser.java
@@ -23,7 +23,9 @@ public class Parser {
   private static final int EXPRARITH = 13;
   private static final int HPPROD = 14;
   private static final int HPEXPR = 15;
+  private static final int HPEXPR_EPSILON = 15;
   private static final int LPEXPR = 16;
+  private static final int LPEXPR_EPSILON = 16;
   private static final int SIMPLEEXPR_VARNAME = 17;
   private static final int SIMPLEEXPR_NUMBER = 18;
   private static final int SIMPLEEXPR_PAREN = 19;
@@ -34,10 +36,13 @@ public class Parser {
   private static final int HPOP_DIVIDE = 24;
   private static final int IF = 25;
   private static final int IFELSE = 26;
+  private static final int IFELSE_EPSILON = 26;
   private static final int COND = 27;
   private static final int PCOND = 28;
-  private static final int HPCOND = 29;
-  private static final int LPCOND = 30;
+  private static final int HPCOND_AND = 29;
+  private static final int HPCOND_EPSILON = 29;
+  private static final int LPCOND_OR = 30;
+  private static final int LPCOND_EPSILON = 30;
   private static final int SIMPLECOND_NOT = 31;
   private static final int SIMPLECOND_COMP = 32;
   private static final int COMP_EQ = 33;
@@ -46,12 +51,12 @@ public class Parser {
   private static final int COMP_LEQ = 36;
   private static final int COMP_LT = 37;
   private static final int COMP_NEQ = 38;
-  private static final int COMP_WHILE = 39;
-  private static final int COMP_FOR = 40;
-  private static final int COMP_PRINT = 41;
-  private static final int COMP_READ = 42;
-  private static final int COMP_EXPLIST = 43;
-  private static final int COMP_EXPLISTEND = 44;
+  private static final int WHILE = 39;
+  private static final int FOR = 40;
+  private static final int PRINT = 41;
+  private static final int READ = 42;
+  private static final int EXPLIST = 43;
+  private static final int EXPLISTEND = 44;
 
 
   public static boolean VERBOSE = false;
@@ -94,6 +99,7 @@ public class Parser {
     compareToken(LexicalUnit.ENDLINE);
     skipEndline();
     variables();
+    nextToken();
     skipEndline();
     code();
     //compareToken(LexicalUnit.ENDPROG);
@@ -108,6 +114,7 @@ public class Parser {
       nextToken();
       varlist();
       compareToken(LexicalUnit.ENDLINE);
+      skipEndline();
     } else {
       System.out.print("VARIABLES_EPSILON" + " ");
       nextToken();
@@ -131,7 +138,6 @@ public class Parser {
       varlistend();
     } else {
      System.out.print("VARLISTEND_EPSILON" + " ");
-     nextToken();
      return;
    }
 
@@ -145,7 +151,7 @@ public class Parser {
       case FOR:
       case PRINT:
       case READ:
-        System.out.print("Code" + " ");
+        System.out.print("CODE" + " ");
         instruction();
         if (lookahead.getType().equals(LexicalUnit.ENDLINE)) {
           nextToken();
@@ -158,10 +164,266 @@ public class Parser {
     }
   }
 
-  private void instruction() {
-    //switch(lookahead.getType()) {
+  private void instruction() throws IOException {
+    switch(lookahead.getType()) {
+      case VARNAME:
+        System.out.print("INSTRUCTION_ASSIGN" + " ");
+        assign();
+        break;
+      case IF:
+        System.out.print("INSTRUCTION_IF" + " ");
+        parse_if();
+        break;
+      case WHILE:
+        System.out.print("INSTRUCTION_WHILE" + " ");
+        parse_while();
+        break;
+      case FOR:
+        System.out.print("INSTRUCTION_FOR" + " ");
+        parse_for();
+        break;
+      case PRINT:
+        System.out.print("INSTRUCTION_PRINT" + " ");
+        parse_print();
+        break;
+      case READ:
+        System.out.print("INSTRUCTION_READ" + " ");
+        parse_read();
+        break;
+    }
+  }
 
-    //}
+  private void assign() throws IOException {
+    System.out.print("ASSIGN" + " ");
+    compareToken(LexicalUnit.VARNAME);
+    nextToken();
+    compareToken(LexicalUnit.ASSIGN);
+    nextToken();
+    exprArith();
+    nextToken();
+    skipEndline();
+    code();
+  }
+
+  private void parse_while() throws IOException {
+
+  }
+
+  private void parse_for() throws IOException {
+
+  }
+
+  private void parse_print() throws IOException {
+
+  }
+
+  private void parse_read() throws IOException {
+    System.out.print("READ" + " ");
+    compareToken(LexicalUnit.READ);
+    nextToken();
+    compareToken(LexicalUnit.LPAREN);
+    nextToken();
+    varlist();
+    compareToken(LexicalUnit.RPAREN);
+    nextToken();
+    skipEndline();
+    code();
+  }
+
+  private void exprArith() throws IOException {
+    System.out.print("EXPRARITH" + " ");
+    hpProd();
+    lpExpr();
+  }
+
+  private void hpProd() throws IOException {
+    System.out.print("HPPROD" + " ");
+    simpleExpr();
+    hpExpr();
+  }
+
+  private void hpExpr() throws IOException {
+    if (lookahead.getType().equals(LexicalUnit.TIMES) ||
+        lookahead.getType().equals(LexicalUnit.DIVIDE)) {
+          System.out.print("HPEXPR" + " ");
+          hpOp();
+          simpleExpr();
+          hpExpr();
+    } else {
+      System.out.print("HPEXPR_EPSILON" + " ");
+      return;
+    }
+  }
+
+  private void lpExpr() throws IOException {
+    if (lookahead.getType().equals(LexicalUnit.PLUS) ||
+        lookahead.getType().equals(LexicalUnit.MINUS)) {
+        lpOp();
+        hpProd();
+    } else {
+      System.out.print("LPEXPR_EPSILON" + " ");
+    }
+  }
+
+  private void simpleExpr() throws IOException {
+    switch(lookahead.getType()) {
+      case VARNAME:
+        System.out.print("SIMPLEEXPR_VARNAME" + " ");
+        nextToken();
+        return;
+      case NUMBER:
+        System.out.print("SIMPLEEXPR_NUMBER" + " ");
+        nextToken();
+        return;
+      case LPAREN:
+        System.out.print("SIMPLEEXPR_PAREN" + " ");
+        nextToken();
+        exprArith();
+        compareToken(LexicalUnit.RPAREN);
+        nextToken();
+        break;
+      case MINUS:
+        System.out.print("SIMPLEEXPR_MINUS" + " ");
+        nextToken();
+        exprArith();
+        break;
+    }
+  }
+
+  private void hpOp() throws IOException {
+    if (lookahead.getType().equals(LexicalUnit.TIMES)) {
+      System.out.print("HPOP_TIMES" + " ");
+      nextToken();
+    } else if (lookahead.getType().equals(LexicalUnit.DIVIDE)) {
+      System.out.print("HPOP_DIVIDE" + " ");
+      nextToken();
+    }
+  }
+
+  private void lpOp() throws IOException {
+    if (lookahead.getType().equals(LexicalUnit.PLUS)) {
+      System.out.print("LPOP_PLUS" + " ");
+      nextToken();
+    } else if (lookahead.getType().equals(LexicalUnit.MINUS)) {
+      System.out.print("LPOP_MINUS" + " ");
+    }
+  }
+
+  private void parse_if() throws IOException {
+    System.out.print("IF" + " ");
+    compareToken(LexicalUnit.IF);
+    nextToken();
+    compareToken(LexicalUnit.LPAREN);
+    nextToken();
+    cond();
+    compareToken(LexicalUnit.RPAREN);
+    nextToken();
+    compareToken(LexicalUnit.THEN);
+    nextToken();
+    code();
+    ifElse();
+    compareToken(LexicalUnit.ENDIF);
+    nextToken();
+    code();
+  }
+
+  private void ifElse() throws IOException {
+    if (lookahead.getType().equals(LexicalUnit.ELSE)) {
+      System.out.print("IFELSE" + " ");
+      compareToken(LexicalUnit.ELSE);
+      nextToken();
+      compareToken(LexicalUnit.ENDLINE);
+      nextToken();
+      code();
+    } else {
+      System.out.print("IFELSE_EPSILON" + " ");
+      return;
+    }
+  }
+
+  private void cond() throws IOException {
+    System.out.print("COND" + " ");
+    pCond();
+    lpCond();
+  }
+
+  private void pCond() throws IOException {
+    System.out.print("PCOND" + " ");
+    simpleCond();
+    hpCond();
+  }
+
+  private void hpCond() throws IOException {
+    if (lookahead.getType().equals(LexicalUnit.AND)) {
+          System.out.print("HPCOND_AND" + " ");
+          compareToken(LexicalUnit.AND);
+          nextToken();
+          simpleCond();
+          hpCond();
+    } else {
+      System.out.print("HPCOND_EPSILON" + " ");
+      return;
+    }
+  }
+
+  private void lpCond() throws IOException {
+    if (lookahead.getType().equals(LexicalUnit.OR)) {
+      System.out.print("LPCOND_OR" + " ");
+      compareToken(LexicalUnit.OR);
+      nextToken();
+      pCond();
+      lpCond();
+    } else {
+      System.out.print("LPCOND_EPSILON" + " ");
+    }
+  }
+
+  private void simpleCond() throws IOException {
+    if (lookahead.getType().equals(LexicalUnit.NOT)) {
+      System.out.print("SIMPLECOND_NOT");
+      compareToken(LexicalUnit.NOT);
+      nextToken();
+      simpleCond();
+    } else {
+      exprArith();
+      comp();
+      exprArith();
+    }
+  }
+
+  private void comp() throws IOException {
+    switch(lookahead.getType()) {
+      case EQ:
+        System.out.print("COMP_EQ");
+        compareToken(LexicalUnit.EQ);
+        nextToken();
+        break;
+      case GEQ:
+        System.out.print("COMP_GEQ");
+        compareToken(LexicalUnit.GEQ);
+        nextToken();
+        break;
+      case GT:
+        System.out.print("COMP_GT");
+        compareToken(LexicalUnit.GT);
+        nextToken();
+        break;
+      case LEQ:
+        System.out.print("COMP_LEQ");
+        compareToken(LexicalUnit.LEQ);
+        nextToken();
+        break;
+      case LT:
+        System.out.print("COMP_LT");
+        compareToken(LexicalUnit.LT);
+        nextToken();
+        break;
+      case NEQ:
+        System.out.print("COMP_NEQ");
+        compareToken(LexicalUnit.NEQ);
+        nextToken();
+        break;
+    }
   }
 
 
