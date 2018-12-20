6281c4cbfa19edb75af72e3174f04c03edf02ad1
Oppac <oppac26@gmail.com>
2018-12-20 22:44:05 +0100

    lol

--- src/CodeGenerator.java
+++ src/CodeGenerator.java
@@ -10,21 +10,34 @@ public class CodeGenerator {
     this.symbolicTable = new LinkedHashMap<String, Integer>();
   }
 
-
   public void generateCode() {
     for (AbstractSyntaxTree child: ast.getChildren()) {
       if (child.getLabel() == "Variables") {
         System.out.println(createVariables(child));
       } else if (child.getLabel() == "Code") {
         for (AbstractSyntaxTree codeChild: child.getChildren()) {
-          if (codeChild.getLabel() == "Assign") {
-            System.out.println("Assssignement");
-          } else if (codeChild.getLabel() == "Read") {
-            System.out.println(generateRead(codeChild));
-          }
+          printCode(codeChild);
         }
       }
     }
+  }
+
+  public void printCode(AbstractSyntaxTree code) {
+    if (code.getLabel() == "Assign") {
+      System.out.println("Assssignement");
+    } else if (code.getLabel() == "Read") {
+      System.out.println(generateRead(code));
+    } else if (code.getLabel() == "Print") {
+      System.out.println(generatePrint(code));
+    } else if (code.getLabel() == "Code") {
+        for (AbstractSyntaxTree codeChild: code.getChildren()) {
+          printCode(codeChild);
+        }
+    }
+  }
+
+  public String computeExprArith(AbstractSyntaxTree exprArith) {
+    return exprArith.getChild(0).getLabel();
   }
 
   public String createVariables(AbstractSyntaxTree vars) {
@@ -49,6 +62,16 @@ public class CodeGenerator {
       llvmCode += "%" + i + "= call i32 @readInt()\n";
       llvmCode += "store i32 %" + i + ", i32* %" + varName + "\n";
       i++;
+    }
+    return llvmCode;
+  }
+
+  public String generatePrint(AbstractSyntaxTree print) {
+    System.out.println("\n<Print>");
+    String llvmCode = "";
+    for (AbstractSyntaxTree child: print.getChildren()) {
+      String varName = computeExprArith(child);
+      llvmCode += "call void @println(i32* %" + varName + ")" + "\n";
     }
     return llvmCode;
   }
